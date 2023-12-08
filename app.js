51b058887a5f88a0e0e19d27a6cec0e6d9e848c6
Sean B <seanb4@hotmail.co.uk>
2023-12-08 21:25:25 +0000

    added a bunch of middlewear for publishing

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const mongoUrl = require(path.join(__dirname, 'mongourl.js'));
-const databaseURL = mongoUrl;
-
-console.log("SSSSSSSSSSSSSSSSS")
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
 
@@ -21,7 +19,10 @@ const expressLayouts = require('express-ejs-layouts')
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = databaseURL;
+
+const mongoUrl = require(path.join(__dirname, 'mongourl.js'));
+const databaseURL = mongoUrl;
+const mongoDB = process.env.MONGODB_URI || databaseURL;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -31,16 +32,33 @@ async function main() {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.set('layout', 'views/layout');
-
-//does this work
 app.use(expressLayouts);
 app.set('layout', 'layout');
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
