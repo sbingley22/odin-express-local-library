9f69a69f2b97f0e305503d74ebe1ce4cd3a588af
Sean B <seanb4@hotmail.co.uk>
2023-12-08 15:05:50 +0000

    version 1 finished

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -43,7 +43,6 @@ exports.author_create_get = (req, res, next) => {
     title: "Create Author",
     author: undefined,
     errors: [],
-
   });
 };
 
@@ -153,11 +152,72 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 //Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update GET");
-})
+  const author = await Author.findById(req.params.id)
+  console.log(author)
+  
+  res.render("author_form", { 
+    title: "Create Author",
+    author: author,
+    errors: [],
+  });
+});
 
 //Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req,res, next) => {
-    res.send("NOT IMPLEMENTED: Author update POST");
-})
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id, // This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Save author.
+      const updatedAuthor = await Author.findByIdAndUpdate(req.params.id, author, {});
+      // Redirect to new author record.
+      res.redirect(updatedAuthor.url);
+    }
+  }),
+];
 
